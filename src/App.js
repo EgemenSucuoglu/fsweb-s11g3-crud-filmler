e8de2739c10184f4ece55c408171091d71ba966d
Egemen <sucuogluegemen1@gmail.com>
2023-05-10 12:53:52 +0300

    görev tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,83 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import FavoriteMovieList from './components/FavoriteMovieList';
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-import axios from 'axios';
+import axios from "axios";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
+  const { push } = useHistory();
+
   useEffect(() => {
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
         setMovies(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
   const deleteMovie = (id) => {
-  }
-
-  const addToFavorites = (movie) => {
-
-  }
-
-  return (
-    <div>
-      <nav className="bg-zinc-800 px-6 py-3">
-        <h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
-      </nav>
-
-      <div className="max-w-4xl mx-auto px-3 pb-4">
-        <MovieHeader />
-        <div className="flex flex-col sm:flex-row gap-4">
-          <FavoriteMovieList favoriteMovies={favoriteMovies} />
-
-          <Switch>
-            <Route path="/movies/edit/:id">
-            </Route>
-
-            <Route path="/movies/:id">
-              <Movie />
-            </Route>
-
-            <Route path="/movies">
-              <MovieList movies={movies} />
-            </Route>
-
-            <Route path="/">
-              <Redirect to="/movies" />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </div>
-  );
+    axios
+      .delete(`http://localhost:9000/api/movies/${id}`)
+      .then((res) => setMovies(res.data))
+      .catch((error) => console.log(error));
+    push("/movies");
+  };
 };
 
+const addToFavorites = (movie) => {
+  if (!favoriteMovies.find((mov) => mov.id === movie.id)) {
+    setFavoriteMovies([...favoriteMovies, movie]);
+  } else {
+    console.log("fav id bulduk");
+  }
+};
+
+return (
+  <div>
+    <nav className="bg-zinc-800 px-6 py-3">
+      <h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
+    </nav>
+
+    <div className="max-w-4xl mx-auto px-3 pb-4">
+      <MovieHeader />
+      <div className="flex flex-col sm:flex-row gap-4">
+        <FavoriteMovieList favoriteMovies={favoriteMovies} />
+
+        <Switch>
+          <Route path="/movies/edit/:id">
+            <EditMovieForm setMovies={setMovies} />
+          </Route>
+          <Route path="/movies/add">
+            <AddMovieForm setMovies={setMovies} />
+          </Route>
+
+          <Route path="/movies/:id">
+            <Movie deleteMovie={deleteMovie} addToFavorites={addToFavorites} />
+          </Route>
+
+          <Route path="/movies">
+            <MovieList movies={movies} />
+          </Route>
+
+          <Route path="/">
+            <Redirect to="/movies" />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
-
